689a42a5e1183dbb3e40451da5230568197e4e7f
DebbySpaninks <dspaninks@gmail.com>
2021-01-14 12:09:11 +0100

    add files

---
+++ opdracht_test-driven-development/find-needle.js
@@ -0,0 +1,6 @@
+// geef 2de paramater mee (zie output findNeedle in test document)
+const findNeedle = function (words, findword) {
+    return words.indexOf(findword);
+};
+
+module.exports = findNeedle;
